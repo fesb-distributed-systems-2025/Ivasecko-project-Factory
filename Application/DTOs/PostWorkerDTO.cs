318313c2f83cf7b9b3569c349adc2ff5ea474d89
ivasecko48 <robertivasecko2001@gmail.com>
2025-12-16 17:11:04 +0100

    HW02-add Worker model and DTOs in App layer

---
+++ Application/DTOs/PostWorkerDTO.cs
@@ -0,0 +1,15 @@
+namespace Application.DTOs
+{
+    public class PostWorkerDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
