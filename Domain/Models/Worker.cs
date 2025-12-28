d0c493b3541bac412c8ee3b9d9e0777ddec3f0ab
ivasecko48 <robertivasecko2001@gmail.com>
2025-12-28 22:37:42 +0100

    HW04: add Email domain model with Worker relations

--- Domain/Models/Worker.cs
+++ Domain/Models/Worker.cs
@@ -8,8 +8,15 @@ namespace Domain.Models
 
         public string Surname { get; set; }
 
-        public int Age { get; set; }
+        public int? Age { get; set; }
+        public string EmailAddress { get; set; }
 
-        public DateTime BirthDate { get; set; }
+        public string? Position { get; set; }
+        public DateTime? BirthDate { get; set; }
+
+        #region Reverse Navigation Properties
+        public ICollection<Email> SentEmails { get; set; }
+        public ICollection<Email> ReceivedEmails { get; set; }
+        #endregion
     }
 }
